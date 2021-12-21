c0c6d84b58971e327cb368d54881b2e0ae2d51c3
Donor <donorfelita@msn.com>
2021-12-21 13:51:12 +0100

    ADD Job crawler with Datai spider and Postgres pipeline

---
+++ jobs_crawler/jobs_crawler/pipelines.py
@@ -0,0 +1,36 @@
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import psycopg2
+
+
+class JobsCrawlerPipeline:
+
+    def __init__(self):
+        self.connection = None
+        self.cur = None
+
+    def open_spider(self, spider):
+        hostname = 'localhost'
+        username = 'donor'
+        password = 'jaimelachatteetlepate'
+        database = 'job_market'
+        self.connection = psycopg2.connect(
+            host=hostname, user=username, password=password,
+            dbname=database)
+        self.cur = self.connection.cursor()
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.connection.close()
+
+    def process_item(self, item, spider):
+        try:
+            self.cur.execute(
+                "INSERT INTO jobs(url, title, company, location, type, industry, text) VALUES(%s,%s,%s,%s,%s,%s,%s)",
+                (item['url'][0], item['title'][0], item['company'][0], item['location'][0], item['type'][0],
+                 item['industry'][0], item['text'][0]))
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+            raise
+        return item
