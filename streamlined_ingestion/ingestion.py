ee22a46f6c3f10fdf135eb4b01aea0acb03c7359
Donor <donorfelita@msn.com>
2021-12-22 02:03:42 +0100

    CREATE streamlined ingestion with pandas

---
+++ streamlined_ingestion/ingestion.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 100)
+
+db_string = "postgresql://donor:jaimelachatteetlepate@localhost:5432/job_market"
+engine = create_engine(db_string)
+
+jobs = pd.read_sql("jobs", engine)
+
+print(jobs.head())
