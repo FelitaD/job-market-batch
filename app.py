15c9de9fe192bb6534a19ce5a63137b9d2f066e6
Donor <donorfelita@msn.com>
2022-02-13 03:21:46 +0100

    CREATE Flask app

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'replace_secret_key'
+api = Api(app)
+
+# jwt = JWT(app, authenticate, identity) # endpoint /auth
+
+jobs = []
+
+
+class Job(Resource):
+
+    def get(self, job_id):
+        job = next(filter(lambda x: x['job_id'] == job_id, jobs), None)
+        return {'job': job}, 200 if job else 404
+
+    def post(self, job_id):
+        if next(filter(lambda x: x['job_id'] == job_id, jobs), None) is not None:
+            return {'message': "A job with id '{}' already exists.".format(job_id)}, 400  # Bad Request, client's fault (should have checked if item existed)
+
+        data = request.get_json()  # gets data from the payload in the request
+        job = {'job_id': job_id, 'remote': data['remote'], 'company': data['company']}
+        jobs.append(job)
+        return job, 201
+
+    def delete(self, job_id):
+        global jobs
+        jobs = list(filter(lambda x: x['job_id'] != job_id, jobs))
+        return {'message': 'Job deleted'}
+
+
+class JobList(Resource):
+    def get(self):
+        return {'jobs': jobs}
+
+
+api.add_resource(Job, '/job/<string:job_id>')
+api.add_resource(JobList, '/jobs')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
