9df36e2f3e6bf5e08d1147fdf2a2178ac1a63919
Donor <donorfelita@msn.com>
2022-02-13 03:22:30 +0100

    DELETE JWT

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required
-
-from security import authenticate, identity
 
 app = Flask(__name__)
-app.secret_key = 'replace_secret_key'
 api = Api(app)
-
-# jwt = JWT(app, authenticate, identity) # endpoint /auth
 
 jobs = []
 
