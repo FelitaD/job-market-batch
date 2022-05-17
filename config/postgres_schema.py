dd7109966a106beb5a099b2f8d6f188a3a69ade5
Donor <donorfelita@msn.com>
2022-05-17 19:07:19 +0200

    Refactor

---
+++ config/postgres_schema.py
@@ -0,0 +1,43 @@
+from numpy.random._generator import Sequence
+from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey, inspect, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pandas as pd
+
+Base = declarative_base()
+
+
+class ProcessedJob(Base):
+    __tablename__ = 'processed_jobs'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String(400), nullable=False, unique=True)
+    title = Column(String(100), nullable=False)
+    company = Column(String(100), nullable=False)
+    industry = Column(String(100))
+    stack = Column(String(100))
+    location = Column(String(100))
+    remote = Column(String(100))
+    type = Column(String(20))
+    language = Column(String(2))
+    created_at = Column(Date)
+    text = Column(String)
+
+
+class RawJob(Base):
+    __tablename__ = 'raw_jobs'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String(400), nullable=False, unique=True)
+    title = Column(String(100), nullable=False)
+    company = Column(String(100), nullable=False)
+    industry = Column(String(100))
+    location = Column(String(100))
+    remote = Column(String(100))
+    type = Column(String(20))
+    created_at = Column(Date)
+    text = Column(String)
+
