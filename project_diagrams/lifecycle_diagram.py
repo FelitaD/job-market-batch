d63d394b59b46659921b9f175b26c0a9fa13116c
Donor <donorfelita@msn.com>
2023-03-19 22:35:11 +0100

    Modify diagram with Airflow

--- project_diagrams/lifecycle_diagram.py
+++ project_diagrams/lifecycle_diagram.py
@@ -3,31 +3,40 @@ from diagrams.onprem.database import Postgresql
 from diagrams.onprem.workflow import Airflow
 from diagrams.programming.language import Python
 from diagrams.onprem.analytics import Tableau
-from diagrams.generic.database import SQL
 from diagrams.custom import Custom
 from diagrams.generic.blank import Blank
 
 
 with Diagram(name='Data lifecycle', outformat="jpg", show=False):
 
-    with Cluster('Storage'):
-        raw = Postgresql('Raw data')
-        processed = Postgresql('Transformed data')
-        b1 = Blank('')
-        b2 = Blank('')
-        e = Edge(color='#E0F4FC')
-        b1 - e - raw - e - processed - e - b2
-
     with Cluster('Data sources'):
         web = Custom('Web', 'web.png')
 
-    with Cluster('Ingestion'):
-        crawler = Python('Crawler')
+    with Cluster(''):
+        with Cluster('Storage'):
+            db = Postgresql('SQL database')
+            b1 = Blank('')
+            b2 = Blank('')
+            e = Edge(color='#E0F4FC')
+            b1 - e - db - e - b2
 
-    with Cluster('Transformation'):
-        etl = Python('ETL pipeline')
+        with Cluster('Ingestion'):
+            crawler = Python('Crawler')
 
-    with Cluster('Serving'):
-        viz = Tableau('Analytics')
+        with Cluster('Transformation'):
+            etl = Python('ETL pipeline')
 
-    web >> crawler >> etl >> viz
+        with Cluster('Serving'):
+            api = Python('REST API')
+
+    with Cluster('Analytics'):
+        viz = Tableau('Tableau')
+
+    airflow = Airflow('Orchestration')
+    b3 = Blank('')
+    b4 = Blank('')
+    b5 = Blank('')
+    e2 = Edge(color='white')
+    b3 - e2 - b4 - e2 - airflow - e2 - b5
+
+    web >> crawler >> etl >> api >> viz
